fba4837559ef298109b77bd10050a7748c07e3ab
AmenAllahMainsi <43909369+AmenAllahMainsi@users.noreply.github.com>
2019-03-17 10:56:39 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo2.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          
+
+          <p className="Introduction">
+                   Edit <code>src/App.js</code> and save to reload.
+          </p>
+
+          <br/>
+
+          <hr/>
+
+          <p className="title">
+                  welcome to react
+          </p>
+           
+        </header>
+
+        </div>
+    );
+  }
+}
+
+export default App;
